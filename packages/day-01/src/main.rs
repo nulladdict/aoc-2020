591a16afbc6099f87c7d0ec1e1d28299c129def7
nulladdict <nulladdict@ya.ru>
2020-12-01 11:05:27 +0500

    feat: add day 1 solutions

--- packages/day-01/src/main.rs
+++ packages/day-01/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::BufRead;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let xs = std::io::stdin()
+        .lock()
+        .lines()
+        .map(|s| s.map(|x| x.parse::<i32>()))
+        .collect::<Result<Result<Vec<_>, _>, _>>()??;
+    println!("{}", part_1(&xs));
+    println!("{}", part_2(&xs));
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn part_1(xs: &[i32]) -> i32 {
+    for (i, x) in xs.iter().enumerate() {
+        for y in xs.iter().skip(i + 1) {
+            if x + y == 2020 {
+                return x * y;
+            }
+        }
+    }
+    panic!();
+}
+
+#[allow(dead_code)]
+fn part_2(xs: &[i32]) -> i32 {
+    for (i, x) in xs.iter().enumerate() {
+        for (j, y) in xs.iter().skip(i + 1).enumerate() {
+            for z in xs.iter().skip(i + j + 1) {
+                if x + y + z == 2020 {
+                    return x * y * z;
+                }
+            }
+        }
+    }
+    panic!();
 }
