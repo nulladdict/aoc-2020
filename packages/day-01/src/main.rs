8fad9a2ac1f91e8933b3ec40085a81c8edee84e3
nulladdict <nulladdict@ya.ru>
2020-12-01 12:05:43 +0500

    fix: make day 1 a little faster

--- packages/day-01/src/main.rs
+++ packages/day-01/src/main.rs
@@ -1,22 +1,28 @@
+use std::cmp::Ordering;
 use std::io::BufRead;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let xs = std::io::stdin()
+    let mut xs = std::io::stdin()
         .lock()
         .lines()
         .map(|s| s.map(|x| x.parse::<i32>()))
         .collect::<Result<Result<Vec<_>, _>, _>>()??;
+    xs.sort();
     println!("{}", part_1(&xs));
     println!("{}", part_2(&xs));
     Ok(())
 }
 
+const YEAR: i32 = 2020;
+
 #[allow(dead_code)]
 fn part_1(xs: &[i32]) -> i32 {
     for (i, x) in xs.iter().enumerate() {
         for y in xs.iter().skip(i + 1) {
-            if x + y == 2020 {
-                return x * y;
+            match (x + y).cmp(&YEAR) {
+                Ordering::Equal => return x * y,
+                Ordering::Less => continue,
+                Ordering::Greater => break,
             }
         }
     }
@@ -27,9 +33,14 @@ fn part_1(xs: &[i32]) -> i32 {
 fn part_2(xs: &[i32]) -> i32 {
     for (i, x) in xs.iter().enumerate() {
         for (j, y) in xs.iter().skip(i + 1).enumerate() {
+            if (x + y) >= YEAR {
+                break;
+            }
             for z in xs.iter().skip(i + j + 1) {
-                if x + y + z == 2020 {
-                    return x * y * z;
+                match (x + y + z).cmp(&YEAR) {
+                    Ordering::Equal => return x * y * z,
+                    Ordering::Less => continue,
+                    Ordering::Greater => break,
                 }
             }
         }
