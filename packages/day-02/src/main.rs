e03f4bfba47ceb4f90629ec526210f2ae8ed1af9
nulladdict <nulladdict@ya.ru>
2020-12-02 15:51:23 +0500

    fix(day-02): better parsing

--- packages/day-02/src/main.rs
+++ packages/day-02/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::io::BufRead;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -5,26 +6,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .lock()
         .lines()
         .collect::<Result<Vec<_>, _>>()?;
-    part_1(&lines)?;
-    part_2(&lines)?;
+    println!("{:?}", part_1(&lines));
+    println!("{:?}", part_2(&lines));
     Ok(())
 }
 
-struct Rule<'a, T: std::str::FromStr>(&'a str, char, T, T);
+struct Rule<'a>(&'a str, char, usize, usize);
 
-fn parse<T: std::str::FromStr>(line: &str) -> Result<Rule<T>, T::Err> {
-    let parts = line.split_ascii_whitespace().collect::<Vec<_>>();
-    let lh = parts[0].split("-").collect::<Vec<_>>();
-    let low = lh[0].parse::<T>()?;
-    let high = lh[1].parse::<T>()?;
-    let letter = parts[1].chars().nth(0).unwrap();
-    let pass = parts[2];
-    Ok(Rule(pass, letter, low, high))
+impl Rule<'_> {
+    const NO_MATCH: &'static str = "NoMatch";
+
+    fn parse(line: &'_ str) -> Result<Rule<'_>, Box<dyn std::error::Error>> {
+        let rule = Regex::new(r"(\d+)-(\d+)\s(\w):\s(\w+)")?;
+        let capture = rule.captures(line).ok_or(Self::NO_MATCH)?;
+        let low = capture[1].parse::<usize>()?;
+        let high = capture[2].parse::<usize>()?;
+        let letter = capture[3].chars().nth(0).ok_or(Self::NO_MATCH)?;
+        let password = capture.get(4).ok_or(Self::NO_MATCH)?.as_str();
+        Ok(Rule(password, letter, low, high))
+    }
 }
 
-fn validate_1(str: &str, letter: char, low: i32, high: i32) -> bool {
+fn validate_1(rule: Rule) -> bool {
+    let Rule(password, letter, low, high) = rule;
     let mut count = 0;
-    for l in str.chars() {
+    for l in password.chars() {
         if l == letter {
             count += 1;
         }
@@ -35,31 +41,28 @@ fn validate_1(str: &str, letter: char, low: i32, high: i32) -> bool {
     count >= low
 }
 
-fn part_1(lines: &[String]) -> Result<(), Box<dyn std::error::Error>> {
+fn part_1(lines: &[String]) -> Result<i32, Box<dyn std::error::Error>> {
     let mut count = 0;
     for line in lines {
-        let Rule(password, letter, low, high) = parse(line)?;
-        if validate_1(password, letter, low, high) {
+        if validate_1(Rule::parse(line)?) {
             count += 1;
         }
     }
-    println!("{}", count);
-    Ok(())
+    Ok(count)
 }
 
-fn validate_2(str: &str, letter: char, low: usize, high: usize) -> bool {
-    let chars: Vec<_> = str.chars().collect();
+fn validate_2(rule: Rule) -> bool {
+    let Rule(password, letter, low, high) = rule;
+    let chars: Vec<_> = password.chars().collect();
     (chars[low - 1] == letter) ^ (chars[high - 1] == letter)
 }
 
-fn part_2(lines: &[String]) -> Result<(), Box<dyn std::error::Error>> {
+fn part_2(lines: &[String]) -> Result<i32, Box<dyn std::error::Error>> {
     let mut count = 0;
     for line in lines {
-        let Rule(password, letter, low, high) = parse(line)?;
-        if validate_2(password, letter, low, high) {
+        if validate_2(Rule::parse(line)?) {
             count += 1;
         }
     }
-    println!("{}", count);
-    Ok(())
+    Ok(count)
 }
